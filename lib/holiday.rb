8624d5c0731266bb610a78b1dca63711817c08ea
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-11-11 21:21:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,6 +75,23 @@ def all_holidays_with_bbq(holiday_hash)
   end.flatten.compact
 end
 
+   {
+      :winter => {
+        :christmas => ["Lights", "Wreath"],
+        :new_years => ["Party Hats"]
+      },
+      :summer => {
+        :fourth_of_july => ["Fireworks", "BBQ"]
+      },
+      :fall => {
+        :thanksgiving => ["Turkey"]
+      },
+      :spring => {
+        :memorial_day => ["BBQ"]
+      }
+    }
+  }
+
 
 
 
